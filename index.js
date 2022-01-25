f73197748655c781a63937d89d003d7489072a8c
jake <jake.is.mybrother@gmail.com>
2022-01-25 12:54:21 +0900

    Cookieの有効期限を設定

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-  res.end(req.headers.cookie);
+  const limit_time = 'Mon, 07 Jan 2036 00:00:00 GMT'
+  res.setHeader('Set-Cookie', `last_access=${now};expires=${limit_time}`);
+  res.end(`last_access=${now};expires=${limit_time}`);
 });
 const port = 8000;
 server.listen(port, () => {
