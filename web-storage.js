106fc3a8dc5a4d47ec5d928ef78fc6573bd01d43
Masatoshi Ishida <masa_14chan@yahoo.co.jp>
2022-05-28 11:39:31 +0900

    Web Storageにキーfavarite、値programmingデータを追加保存

--- web-storage.js
+++ web-storage.js
@@ -4,3 +4,5 @@ const key = 'last_access';
 localStorage.setItem(key, now);
 const value = localStorage.getItem(key);
 console.log(value);
+localStorage.setItem('favarite', 'programming');
+console.log(localStorage.getItem('favarite'));
