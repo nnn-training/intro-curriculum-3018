f2f57068e35b44b74bb73cec143410980877790e
Tanaka <f.tanaka@gftd.works>
2022-06-20 15:07:46 +0900

    WebStorageにfavoriteの値を保存、その値をコンソールに表示

--- web-storage.js
+++ web-storage.js
@@ -4,3 +4,8 @@ const key = 'last_access';
 localStorage.setItem(key, now);
 const value = localStorage.getItem(key);
 console.log(value);
+
+const key2 = 'favorite';
+localStorage.setItem(key2, 'programming');
+const value2 = localStorage.getItem(key2);
+console.log(value2);
