745426057cefe8b604478d1abc9d02de8adfb48d
Akina Gerber <akina_gerber@dwango.co.jp>
2022-11-16 08:41:35 +0900

    cookieに有効期限を追加

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  
   const now = Date.now();
+  const expDate = new Date(now + 30 * 3600*24 * 1000);
+  const expDateStr = expDate.toUTCString();
+
   res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-  res.setHeader('Set-Cookie', `last_access=${now};`);
+  res.setHeader('Set-Cookie', `last_access=${now};expires=${expDateStr};`);
   const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
   res.end(new Date(last_access_time).toString());
 });
