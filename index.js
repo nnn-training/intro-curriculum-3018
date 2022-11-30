c2c1a3a4a43fb9daf6e257db07ff4fb21a29b22e
Jun <jun@example.com>
2022-11-30 15:00:29 +0900

    Set expire on the last_access cookie

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  const now = Date.now();
   res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-  res.setHeader('Set-Cookie', `last_access=${now};`);
+  
+  const now = Date.now();
+  // 30 days * 24 h/day * 3600 seconds/hour * 1000 milliseconds/second
+  const expireDate = new Date(now + 30 * 24 * 3600 * 1000);
+  res.setHeader('Set-Cookie', `last_access=${now};expires=${expireDate.toUTCString()}`);
+  
   const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
   res.end(new Date(last_access_time).toString());
 });
