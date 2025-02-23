5f579d7f900dcb089fc7ad4c88fc94f3912cd4ba
lgpq <186059148+lgpq@users.noreply.github.com>
2025-02-23 18:12:59 +0900

    Cookie の有効期限を30日後に設定

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const http = require('node:http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
+  const expiresDate = new Date(now + 30 * 86400 * 1000);
   res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-  res.setHeader('Set-Cookie', `last_access=${now};`);
-  const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+  res.setHeader('Set-Cookie', `last_access=${now};expires=${expiresDate.toUTCString()};`);
+  const last_access_time = req.headers.cookie
+    ? parseInt(req.headers.cookie.split('last_access=')[1])
+    : now;
   res.end(new Date(last_access_time).toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
 });
 const port = 8000;
