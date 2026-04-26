2d7acfa83433ab7cd5668e57aae955a265e55500
251zs02244-cell <>
2026-04-26 13:15:01 +0900

    30日後にexpireする設定を追加

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const http = require('node:http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
+  const expiresDate = new Date(now + 30 * 24 * 60 * 60 * 1000);
   res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+  res.setHeader('Set-Cookie', `last_access=${now};expires=${expiresDate.toUTCString()};`);
   res.setHeader('Set-Cookie', `last_access=${now};`);
   const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
   res.end(new Date(last_access_time).toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
